2f9bf529ade5d1f12d566d8342d256cdf2f64016
thechicketarian <thechicketarian@gmail.com>
2022-06-25 12:58:35 -0500

    Uploading Models

---
+++ main/models/jobs.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+ 
+const sequelize = require('../config/connection.js');
+const bcrypt = require('bcrypt');
+ 
+class Jobs extends Model {
+}
+ 
+Jobs.init(
+ {
+   // define columns
+    //this data will be used once they hit [post a job button] under the company user login
+   id: { //autopopulates
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     AutoIncrement: true,
+   },
+   job_title: { //name of the title they are looking to hire
+       type: DataTypes.STRING,
+       allowNull: false,
+   },
+   company_name: { //name of the company posting the job. specified once the user logs in.
+       type: DataTypes.INTEGER,
+       allowNull: false,
+   },
+   role_id: { // this dictates what you as a company user are looking to hire. You choose from a dropdown that references the roles table (on frontend)
+     type: DataTypes.STRING,
+     allowNull: false,
+     references: {
+        model: 'roles',
+        keys: 'id',
+    }
+   },
+   user_id: { //references the current logged in user by ID via the user model
+       type: DataTypes.INTEGER,
+       references: {
+           model: 'user',
+           keys: 'id',
+       }
+   },
+ 
+},
+ 
+);
+ 
+module.exports = Jobs;
---
+++ main/models/roles.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+ 
+const sequelize = require('../config/connection.js');
+const bcrypt = require('bcrypt');
+ 
+class Roles extends Model {
+}
+ 
+//this table allows us to control the jobRole input on the frontend. So they will be able to choose form a dropdown what role they are looking for (company user) or they qualify for (applicant user)
+Roles.init(
+ {
+   // define columns
+   id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     AutoIncrement: true,
+   },
+  role_name: {
+       type: DataTypes.STRING,
+       allowNull: false,
+   }
+
+},
+ 
+);
+ 
+module.exports = Roles;
