8b0b4a64f1e86bff517a7c8f1b3fc5ddaa1f3ee2
Rocket Appliances <monaco.vin@gmail.com>
2022-06-26 16:18:07 -0500

    edited env connections server and created public folder containing css folder img folder js folder

---
+++ main/config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+let sequelize;
+if (process.env.JAWSDB) {
+  sequelize = new Sequelize(process.env.JAWSDB);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+module.exports = sequelize;
