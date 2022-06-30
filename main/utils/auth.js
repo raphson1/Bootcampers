b78a6d078f87fd895bd1bfb772c287c865a00961
raphson1 <raphsoneric@gmail.com>
2022-06-29 20:50:20 -0500

    utils

---
+++ main/utils/auth.js
@@ -0,0 +1,11 @@
+const { default: ModelManager } = require("sequelize/types/model-manager");
+
+const withAuth = (req, res, next) => {
+    if(!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next()
+    }
+}
+
+module.exports = withAuth;
