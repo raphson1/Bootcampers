62666c119a81103d1a74da3f5540f5e6f0bc2dd8
Rocket Appliances <monaco.vin@gmail.com>
2022-07-05 17:37:39 -0500

    home-routes applicant-routes, applicant hb

--- controllers/api/applicant-routes.js
+++ controllers/api/applicant-routes.js
@@ -1,32 +1,42 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const { Applicant } = require('../../models');
+const { Applicant } = require("../../models");
 
 // find all jobs
-router.get('/', async (req, res) => {
-    try {
-        const applicantionData = await Applicant.findAll({});
+router.get("/", async (req, res) => {
+  try {
+    const applicantionData = await Applicant.findAll({});
 
     console.log("Checking to see if this work", applicantionData);
-     res.status(200).json(applicantionData);
-    } catch(err) {
-      res.status(500).json(err);
-    }
-    });
+    res.status(200).json(applicantionData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // post new application
-router.post('/', async (req, res) => {
-    try {
-      const newApplication = await Applicant.create({...req.body // ONCE AUTHENTICATION IS HERE, user_id: req.session.user_id,
-      });
-      console.log("Checking to see if this work", newApplication);
-      res.status(200).json(newApplication);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.post("/", async (req, res) => {
+  try {
+    const newApplication = await Applicant.create({
+      ...req.body, // ONCE AUTHENTICATION IS HERE, user_id: req.session.user_id,
+    });
+    console.log("Checking to see if this work", newApplication);
+    res.status(200).json(newApplication);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-  //delete an app
+// router.get("/applicants", async (req, res) => {
+//   console.log("Group two");
+//   if (req.session.logged_in) {
+//     const applicantData = await Applicant.findAll();
+//     console.log(applicantData);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+//delete an app
 // router.delete('/:id', async (req, res) => {
 //   try {
 //     const applicantionData = await Jobs.destroy({
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,44 +1,46 @@
-const router = require('express').Router();
-const { User, Applicant } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Applicant } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    // Pass serialized data into Handlebars.js template
-    res.render('homepage');
-  }
-);
-
+router.get("/", async (req, res) => {
+  // Pass serialized data into Handlebars.js template
+  res.render("homepage");
+});
+router.get("/applicants", withAuth, async (req, res) => {
+  const applicantData = await Applicant.findAll();
+  const applicants = applicantData.get({ plain: true });
+  return res.send("hi");
+});
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Applicant }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
+      // similar thing need to be created as in line 12 like "fetched applicants page" and replaced with "res.random"
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
-
